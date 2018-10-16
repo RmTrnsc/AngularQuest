5cdc71c3f73ffd312e20e12fca68ca042d3a9cf5
RmTrnsc <tournesacromain@gmail.com>
2018-10-16 12:01:08 +0200

    ajout des routers

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent} from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserProfilComponent,
+    MenuComponent,
     SignUpComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        {path: "sign-up", component: SignUpComponent, pathMatch: "full"},
+        {path: "menu", component: MenuComponent, pathMatch: "full",}
+        {path: "user-profil", component: UserProfilComponent, pathMatch: "full"},
+        {path: "", redirectTo: "user-profil", pathMatch: "full"}
+      ],
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
